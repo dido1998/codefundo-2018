7be9b5026fd5c901ff1e81ff6acce21bf991aa6b
naruarjun <naruarjun@gmail.com>
2018-10-29 18:21:48 +0530

    adding new API key

--- FlaskApp/appC/tasks/twitterinterface/geocoding.py
+++ FlaskApp/appC/tasks/twitterinterface/geocoding.py
@@ -1,14 +1,14 @@
 import urllib.request
 import json
 def geocode(address):
-	response=urllib.request.urlopen('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key=AIzaSyC-WytuJY84cvqRxPC7JSJ2BuDUL6rWbpk')
+	response=urllib.request.urlopen('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key=AIzaSyAspEXV0YGLiWuCke7bsbERDuzbzxARtVQ')
 	html=response.read()
 	html=html.decode("utf-8") 
 	res=json.loads(html)
 	return res['results'][0]['geometry']['location']['lat'],res['results'][0]['geometry']['location']['lng']
 
 def reversegeocode(lat,lon):
-	response=urllib.request.urlopen('https://maps.googleapis.com/maps/api/geocode/json?latlng='+str(lat)+','+str(lon)+'&key=AIzaSyC-WytuJY84cvqRxPC7JSJ2BuDUL6rWbpk')
+	response=urllib.request.urlopen('https://maps.googleapis.com/maps/api/geocode/json?latlng='+str(lat)+','+str(lon)+'&key=AIzaSyAspEXV0YGLiWuCke7bsbERDuzbzxARtVQ')
 	html=response.read()
 	html=html.decode("utf-8") 
 	res=json.loads(html)
